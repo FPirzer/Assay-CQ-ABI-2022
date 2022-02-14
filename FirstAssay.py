4312405557854074c54ae5aeb4e9a479fb730da9
Seminar Bioinformatik <serebix@gmx.de>
2022-02-14 09:51:14 +0100

    Added more lists

--- FirstAssay.py
+++ FirstAssay.py
@@ -1,2 +1,12 @@
-Assay1 = ["Item1", "Item2", "Item3"]
-print(Assay1)
+AssayMix = ["Item", "2", "Item3"]
+print(AssayMix)
+
+AssayNumbers1 = ["1", "2", "3"]
+print(AssayNumbers1)
+AssayNumbers2 = ["4", "5", "6"]
+print(AssayNumbers2)
+
+AssayString1 = ["Water", "Eggs", "Sugar"]
+print(AssayString1)
+AssayString2 = ["Salt", "Pepper", "Cheese"]
+print(AssayString2)
